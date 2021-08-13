fb7a1421dd05cb3537099bd627e9971b1d3a64b3
Jan <janbruckner@gmail.com>
2021-08-13 17:57:29 +0200

    Add missing spaces in help texts

--- garmin_uploader/cli.py
+++ garmin_uploader/cli.py
@@ -12,7 +12,7 @@ def main():
     parser = argparse.ArgumentParser(
       formatter_class=argparse.RawDescriptionHelpFormatter,
       description='A script to upload .TCX, .GPX, and .FIT'
-                  'files to the Garmin Connect web site.',
+                  ' files to the Garmin Connect web site.',
       epilog=open(os.path.join(base_dir, 'help.txt')).read(),
     )
 
@@ -21,21 +21,21 @@ def main():
         type=str,
         nargs='+',
         help='Path and name of file(s) to upload, list file name, or directory'
-             'name containing fitness files.')
+             ' name containing fitness files.')
     parser.add_argument(
         '-a',
         '--name',
         dest='activity_name',
         type=str,
         help='Sets the activity name for the upload file. This option is'
-             'ignored if multiple upload files are given.')
+             ' ignored if multiple upload files are given.')
     parser.add_argument(
         '-t',
         '--type',
         dest='activity_type',
         type=str,
         help='Sets activity type for ALL files in filename list, except files'
-             'described inside a csv list file.')
+             ' described inside a csv list file.')
     parser.add_argument(
         '-u',
         '--username',
